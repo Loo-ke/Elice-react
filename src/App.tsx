43aad17d49ac7f6074725c9866e42f9b2f605386
Jonguk Lee <looke2930@gmail.com>
2023-05-11 00:04:20 +0900

    prettier설정, Counter2-현재시간 출력해주는 컴포넌트 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
 type CounterProps = {
   title: string;
   initValue?: number;
@@ -20,10 +19,38 @@ function Counter({ title, initValue = 0 }: CounterProps) {
     </>
   );
 }
+function getCurrentTime() {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+
+  return `${hours}:${minutes}:${seconds}`;
+}
+
+console.log(getCurrentTime());
+
+function Counter2() {
+  const [times, setTimes] = useState([]);
+  console.log('component - times', times);
+  function up() {
+    const newTimes = [...times];
+    newTimes.push(getCurrentTime());
+    setTimes(newTimes);
+  }
+  return (
+    <>
+      <h1>카운터2</h1>
+      <button onClick={up}>+</button>
+      <ol></ol>
+    </>
+  );
+}
 function App() {
   return (
     <div>
       <Counter title="불면증 카운터" initValue={0}></Counter>
+      <Counter2></Counter2>
     </div>
   );
 }
