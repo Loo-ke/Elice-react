8e2fd5559c0cefb7165de9b260a670d422b80e28
Jonguk Lee <looke2930@gmail.com>
2023-05-18 01:01:25 +0900

    toggle 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import React, { useState } from "react";
 import "./App.css";
+import { Container } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
 
 type countItemType = {
   time: string;
   step: number;
 };
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 function Counter() {
   const [step, setStep] = useState(1);
   const [count, setCount] = useState<countItemType[]>([]);
   return (
-    <div>
+    <div
+      style={{
+        border: "10px solid" + getRandomColor(),
+        margin: "20px 0",
+        backgroundColor: getRandomColor(),
+        color: getRandomColor(),
+      }}
+    >
       <h1>Counter</h1>
       <input
         type="number"
@@ -18,7 +36,8 @@ function Counter() {
           setStep(parseInt(e.target.value));
         }}
       />
-      <button
+      <Button
+        variant="contained"
         onClick={() => {
           const newCountItem: countItemType = {
             time: new Date().toLocaleTimeString(),
@@ -28,8 +47,10 @@ function Counter() {
         }}
       >
         +
-      </button>
-      0{" "}
+      </Button>{" "}
+      {count.reduce((prev, curr) => {
+        return prev + curr.step;
+      }, 0)}
       <table>
         <thead>
           <tr>
@@ -40,7 +61,7 @@ function Counter() {
         <tbody>
           {count.map((value, index) => {
             return (
-              <tr>
+              <tr key={index}>
                 <td>{value.time}</td>
                 <td>{value.step}</td>
               </tr>
@@ -51,12 +72,49 @@ function Counter() {
     </div>
   );
 }
+function Counter2() {
+  const [open, setOpen] = useState(false);
+  return (
+    <div style={{ border: "10px solid black", padding: 20 }}>
+      <h1>Counter - Dialog</h1>
+      <Button
+        variant="contained"
+        onClick={() => {
+          setOpen(true);
+        }}
+      >
+        Run
+      </Button>
 
+      <div style={{ display: open ? "block" : "none" }}>
+        <h2>Dialog Counter</h2>
+        <button>+</button> 0
+        <Button
+          onClick={() => {
+            setOpen(false);
+          }}
+        >
+          close
+        </Button>
+      </div>
+    </div>
+  );
+}
 function App() {
   return (
-    <div>
-      <Counter />
-    </div>
+    <Container>
+      <Grid container spacing={1}>
+        <Grid item xs={12} sm={6} md={4}>
+          <Counter2></Counter2>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Counter></Counter>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Counter></Counter>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
