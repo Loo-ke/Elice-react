81b9081c036434f3e04d684fbcc446e363e5d7d5
Jonguk Lee <looke2930@gmail.com>
2023-05-17 23:23:53 +0900

    input 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function Counter() {
   return (
     <div>
       <h1>Counter</h1>
+      <input type="number" />
       <button>+</button> 0
       <table>
         <thead>
