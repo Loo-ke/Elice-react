27b9b8dceddd5e99d321526856694e41d158683f
Jonguk Lee <looke2930@gmail.com>
2023-05-17 23:22:44 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function Counter() {
+  return (
+    <div>
+      <h1>Counter</h1>
+      <button>+</button> 0
+      <table>
+        <thead>
+          <tr>
+            <td>time</td>
+            <td>step</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Counter />
     </div>
   );
 }
